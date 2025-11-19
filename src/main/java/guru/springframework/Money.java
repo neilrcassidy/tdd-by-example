01a0c3c6f3d73c9153b2393314cd230e6a605261
nlrd <nlrd@gft.com>
2025-11-19 13:43:57 +0100

    added Money class with dollars and francs, started on Bank, Sum and Expression

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,47 @@
+package guru.springframework;
+
+public class Money implements Expression {
+
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    protected String currency() {
+        return this.currency;
+    }
+
+    public Money times(int multiplier){
+        return new Money(this.amount * multiplier, this.currency);
+    }
+
+    @Override
+    public boolean equals(Object object){
+        Money money = (Money) object;
+        return this.amount == money.amount
+                && this.currency.equals(money.currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Expression plus(Money addend){
+        return new Money(this.amount + addend.amount, this.currency);
+    }
+}
---
+++ src/main/java/guru/springframework/Sum.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+public class Sum {
+    Money augmend;
+    Money addmend;
+
+    public Sum(Money augmend, Money addmend) {
+        this.augmend = augmend;
+        this.addmend = addmend;
+    }
+}
